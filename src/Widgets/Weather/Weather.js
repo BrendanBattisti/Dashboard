5601d93d17a1954978b162224e716aa3e2432c34
Brendan Battisti <brendan.battisti@gmail.com>
2023-06-12 14:46:32 -0400

    Updated weather module to more in depth display for days

--- src/Widgets/Weather/Weather.js
+++ src/Widgets/Weather/Weather.js
@@ -1,64 +1,78 @@
 import { useEffect, useState } from "react";
 import styles from "./weather.module.css";
 import axios from "axios";
+import { Collapse } from "@mui/material";
 
 export default function Weather() {
-  const [weatherData, setWeatherData] = useState([]);
-  const [visible, setVisibile] = useState([])
 
-  const weekdays = ["Mon", "Tue", "Wed", "Thur", "Fkri", "Sat", "Sun"];
+
+  const [weatherData, setWeatherData] = useState([]);
+  const [active, setActive] = useState(0)
+
+  const weekdays = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios("weather");
-      let data = response.data.map(Day);
-      setWeatherData(data);
-
-      if (visible.length === 0) {
-        const length = data.length
-        setVisibile(Array.from({ length }, () => false))
-      }
+      setWeatherData(response.data)
     };
+
     const interval = setInterval(() => {
       fetchData();
     }, 1000 * 60 * 1); // Refreshes the weather
     fetchData();
-  }, [visible]);
+
+  }, []);
+
 
   function Hour(weather) {
 
-    //const onClick = () => setVisible(!visible)
+    let time = new Date(weather.dt * 1000).getHours()
 
-    //return (<div onClick={onClick}>Hello there</div>)
+    time = (time > 12 ? time - 12: time) + (time > 12 ? "PM": "AM")
+
+
+    return (
+    <div className={styles.hour}>
+      <div>
+      {Math.round(weather.temp)}
+      </div>
+      <span className="material-symbols-outlined">{weather.weather}</span>
+      <div>
+        {time}
+      </div>
+    </div>
+    )
+
   }
 
   function Day(weather) {
+
     const index = weather.index
 
     const onClick = () => {
-      const tmpVisible = visible
-      tmpVisible[index] = !tmpVisible[index]
-      setVisibile(tmpVisible)
-      console.log(visible)
+
+      if (active === index){
+        setActive(-1)
+      } else {
+        setActive(index)
+      }
+
     }
 
-    const high = Math.round(weather.high);
-    const low = Math.round(weather.low);
-
-    const weather_type = weather.weather;
-    const day = weather.weekday;
     return (
-      <div key={index}>
-        <div className={styles.chunk} onClick={onClick}>
-          <div>{weekdays[day]}</div>
+      <div key={weather.index}>
+        <div className={styles.chunk} onClick={onClick} >
+          <div>{weekdays[weather.weekday]}</div>
           <div className={styles.temp}>
-            H: {high}&deg; L: {low}&deg; {visible[index] ? "Penis" : null}
+            H: {Math.round(weather.high)}&deg; L: {Math.round(weather.low)}&deg;
           </div>
-          <div>
-            <span className="material-symbols-outlined">{weather_type}</span>
-          </div>
-
+          <span className="material-symbols-outlined">{weather.weather}</span>
         </div>
-
+        <Collapse in={active === index}>
+          <div className={styles.hourly}>
+          {weather.chunks.map(Hour)}
+          </div>
+        </Collapse>
       </div>
     );
   }
@@ -66,7 +80,7 @@ export default function Weather() {
   // Add humidity to each weather chunk
   return (
     <div className={styles.weather}>
-      <div className={styles.stream}>{weatherData}</div>
+      <div className={styles.stream}>{weatherData.map(Day)}</div>
     </div>
   );
 }
