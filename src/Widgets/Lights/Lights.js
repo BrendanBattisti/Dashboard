640f8618735fedf31959e56c5d0b6d19fe8c7c51
Brendan Battisti <brendan.battisti@gmail.com>
2023-08-13 23:27:59 -0400

    Redesigned the lights list

--- src/Widgets/Lights/Lights.js
+++ src/Widgets/Lights/Lights.js
@@ -34,21 +34,24 @@ export default function Lights() {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  function LightSwitch(data) {
+  function LightSwitch(data, index) {
     return (
-      <div key={data.name}>
+      <div
+        key={index}
+        className={index % 2 == 0 ? styles.darkitem : styles.lightitem}
+      >
+        {capitalizeFirstLetter(data.name)}
         <Switch
           checked={data.on}
           onClick={() => ToggleLight(data.name, data.on)}
         />
-        {capitalizeFirstLetter(data.name)}
       </div>
     );
   }
 
-  return (
-    <div className={styles.lights}>
-      <div className={styles.lights_list}>
+  const old = (
+    <div className={styles.container}>
+      <div className={styles.itemContainer}>
         {data
           .filter((element) => {
             if (filter === "") {
@@ -61,18 +64,87 @@ export default function Lights() {
           })
           .map(LightSwitch)}
       </div>
-      <div className={styles.controlBlock}>
+      <div className={styles.controlInput}>
+        <input
+          id="search"
+          label="Search"
+          key="searchbar"
+          placeholder="Search"
+          onChange={(event) => setFilter(event.target.value)}
+          className={styles.itemInput}
+        />
+        <button
+          variant="contained"
+          className="material-symbols-outlined"
+          style={{
+            transition: "text-shadow 0.3s ease-in-out", // Apply transition to the text-shadow property
+          }}
+          onMouseEnter={(e) => {
+            e.currentTarget.style.textShadow = "0 0 6px rgba(0, 255, 255, 0.8)"; // Faint glow on hover
+          }}
+          onMouseLeave={(e) => {
+            e.currentTarget.style.textShadow = "none"; // Remove the glow when not hovering
+          }}
+        >
+          refresh
+        </button>
+      </div>
+    </div>
+  );
+  return old;
+  const lights = (
+    <div className={styles.container}>
+      <div className={styles.itemContainer}>
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className={index % 2 == 0 ? styles.darkitem : styles.lightitem}
+          >
+            {item}
+            <button className={styles.deleteButton}>
+              <span
+                className="material-symbols-outlined"
+                style={{
+                  transition: "text-shadow 0.3s ease-in-out", // Apply transition to the text-shadow property
+                }}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.textShadow =
+                    "0 0 6px rgba(0, 255, 255, 0.8)"; // Faint glow on hover
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.textShadow = "none"; // Remove the glow when not hovering
+                }}
+              >
+                delete
+              </span>
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className={styles.controlInput}>
         <input
           id="search"
           label="Search"
           key="searchbar"
           onChange={(event) => setFilter(event.target.value)}
         />
-        <div className={styles.buttonContainer}>
-          <Button variant="contained" className="material-symbols-outlined">
-            refresh
-          </Button>
-        </div>
+        <button>
+          <span
+            className="material-symbols-outlined"
+            style={{
+              transition: "text-shadow 0.3s ease-in-out", // Apply transition to the text-shadow property
+            }}
+            onMouseEnter={(e) => {
+              e.currentTarget.style.textShadow =
+                "0 0 6px rgba(0, 255, 255, 0.8)"; // Faint glow on hover
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.textShadow = "none"; // Remove the glow when not hovering
+            }}
+          >
+            add
+          </span>
+        </button>
       </div>
     </div>
   );
