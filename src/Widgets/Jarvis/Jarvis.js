e192ac630f8acce38c2d128dfe0f597f851f5572
Brendan battist <brendan.battisti@gmail.com>
2023-08-18 11:25:53 -0400

    Progress on random animations

--- src/Widgets/Jarvis/Jarvis.js
+++ src/Widgets/Jarvis/Jarvis.js
@@ -15,19 +15,30 @@ export default function Jarvis() {
       style={{ width: get_size(0), height: get_size(0) }}
     ></div>
   );
+  const borderColor = () => {
+    return Math.round(Math.random() * 3) === 1 ? "rgba(0, 238, 255)" : null;
+  };
+
   function Edge(contents, size) {
     if (size === 0) {
       return contents;
     }
+    console.log(
+      `standard_rotation ${Math.round(Math.random() * 20)}s infinite reverse`
+    );
     return (
       <div
         className={styles.edge}
         style={{
           width: get_size(size),
           height: get_size(size),
-          animation: `standard_rotation ${
+          animation: `standard_rotation ${Math.round(
             Math.random() * 20
-          }s infinite reverse`,
+          )}s infinite reverse`,
+          borderTopColor: borderColor(),
+          borderRightColor: borderColor(),
+          borderBottomColor: borderColor(),
+          borderLeftColor: borderColor(),
         }}
       >
         {Edge(contents, size - 2)}
